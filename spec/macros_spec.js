d74da448f1c9e2a8a79d73df10dfd9d461629772
Andrew Peterson <ndp@mac.com>
2010-09-21 21:58:33 -0700

    add safari rounded corners

--- spec/macros_spec.js
+++ spec/macros_spec.js
@@ -1,20 +1,20 @@
 describe('macros', function() {
     describe('roundedCorners', function() {
         it('should provide a default curvature of 10', function() {
-            expect(roundedCorners()).toEqual({ '-moz-border-radius' : 10, 'border-radius' : 10 });
+            expect(roundedCorners()).toEqual({ '-moz-border-radius' : 10, 'border-radius' : 10, '-webkit-border-radius' : 10 });
         });
         it('should respect input radius', function() {
-            expect(roundedCorners(5)).toEqual({ '-moz-border-radius' : 5, 'border-radius' : 5 });
+            expect(roundedCorners(5)).toEqual({ '-moz-border-radius' : 5, 'border-radius' : 5, '-webkit-border-radius' : 5 });
         });
         it('should curve all corners when asked explicitly', function() {
-            expect(roundedCorners('all', 7)).toEqual({ '-moz-border-radius' : 7, 'border-radius' : 7 });
+            expect(roundedCorners('all', 7)).toEqual({ '-moz-border-radius' : 7, 'border-radius' : 7, '-webkit-border-radius' : 7 });
         });
         it('should curve just top left corner', function() {
-            expect(roundedCorners('tl', 7)).toEqual({ '-moz-border-radius-topleft' : 7, 'border-top-left-radius' : 7 });
+            expect(roundedCorners('tl', 7)).toEqual({ '-moz-border-radius-topleft' : 7, 'border-top-left-radius' : 7, '-webkit-border-top-left-radius' : 7 });
         });
-        it('should curve just top side corner', function() {
-            expect(roundedCorners('top', 7)).toEqual({ '-moz-border-radius-topleft' : 7, 'border-top-left-radius' : 7,
-            '-moz-border-radius-topright' : 7, 'border-top-right-radius' : 7});
+        it('should curve both top side corners', function() {
+            expect(roundedCorners('top', 7)).toEqual({ '-moz-border-radius-topleft' : 7, 'border-top-left-radius' : 7, '-webkit-border-top-left-radius' : 7,
+            '-moz-border-radius-topright' : 7, 'border-top-right-radius' : 7, '-webkit-border-top-right-radius' : 7});
         });
     });
 
--- src/macros/macros.js
+++ src/macros/macros.js
@@ -21,23 +21,28 @@ function roundedCorners(side, radius) {
     if (side == 'all') {
         return {
             '-moz-border-radius': radius,
-            'border-radius': radius
+            'border-radius': radius,
+            '-webkit-border-radius': radius
         }
     } else {
         var rules = {};
         if (side == 'tl' || side == 'top' || side == 'left') {
             rules['-moz-border-radius-topleft'] = radius;
+            rules['-webkit-border-top-left-radius'] = radius;
             rules['border-top-left-radius'] = radius;
         }
         if (side == 'tr' || side == 'top' || side == 'right') {
+            rules['-webkit-border-top-right-radius'] = radius;
             rules['-moz-border-radius-topright'] = radius;
             rules['border-top-right-radius'] = radius;
         }
         if (side == 'bl' || side == 'bottom' || side == 'left') {
+            rules['-webkit-border-bottom-left-radius'] = radius;
             rules['-moz-border-radius-bottomleft'] = radius;
             rules['border-bottom-left-radius'] = radius;
         }
         if (side == 'br' || side == 'bottom' || side == 'right') {
+            rules['-webkit-border-bottom-right-radius'] = radius;
             rules['-moz-border-radius-bottomright'] = radius;
             rules['border-bottom-right-radius'] = radius;
         }
