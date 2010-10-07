c67cc083af01b40b36f43642e5c9b450040bc8bf
Andrew Peterson <ndp@mac.com>
2010-10-07 13:40:03 -0700

    functionality to determine user agent taken from jQuery

--- spec/utils_spec.js
+++ spec/utils_spec.js
@@ -82,4 +82,32 @@ describe('mergeHashInto', function() {
         mergeHashInto(h, {propB: 2}, {propC: 3});
         expect(h).toEqual({propA: 1, propB: 2, propC: 3});
     });
+
+});
+
+describe('userAgent', function() {
+
+    it("should report that it's running in a browser", function() {
+        expect(Csster.browser).not.toBeUndefined();
+    });
+    if (/AppleWebKit/.test(AppleWebKit)) {
+        it("should report that it's running in webkit", function() {
+            expect(Csster.browser.webkit).not.toBeUndefined();
+        });
+        it("should report that it's not running in firefox", function() {
+            expect(Csster.browser.mozilla).toBeUndefined();
+        });
+    }
+    if (/^Mozilla/.test(AppleWebKit)) {
+        it("should report that it's not running in webkit", function() {
+            expect(Csster.browser.webkit).toBeUndefined();
+        });
+        it("should report that it's running in firefox", function() {
+            expect(Csster.browser.mozilla).not.toBeUndefined();
+        });
+    }
+    it("should report that it's not running in opera", function() {
+        expect(Csster.browser.opera).toBeUndefined();
+    });
+
 });
--- src/core.js
+++ src/core.js
@@ -8,6 +8,31 @@ if (!Csster) {
     }
 }
 
+Csster.browser = {};
+
+(function() {
+	// Lifted from jQuery: http://docs.jquery.com/Utilities/jQuery.browser
+	function uaMatch( ua ) {
+		ua = ua.toLowerCase();
+
+		var match = /(webkit)[ \/]([\w.]+)/.exec( ua ) ||
+			/(opera)(?:.*version)?[ \/]([\w.]+)/.exec( ua ) ||
+			/(msie) ([\w.]+)/.exec( ua ) ||
+			!/compatible/.test( ua ) && /(mozilla)(?:.*? rv:([\w.]+))?/.exec( ua ) ||
+		  	[];
+
+		return { browser: match[1] || "", version: match[2] || "0" };
+	}
+
+
+    var browserMatch = uaMatch(navigator.userAgent);
+    if ( browserMatch.browser ) {
+        Csster.browser[ browserMatch.browser ] = true;
+        Csster.browser.version = browserMatch.version;
+    }
+})();
+
+
 Csster.propertyNames = ['accelerator',
     'azimuth',
     'background',
