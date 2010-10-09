c13fbd0f2683d91cd6d1da9c43b7c78cb8cd9f6c
Andrew Peterson <ndp@mac.com>
2010-10-08 19:56:25 -0700

    fix browser tests

--- spec/utils_spec.js
+++ spec/utils_spec.js
@@ -90,7 +90,7 @@ describe('userAgent', function() {
     it("should report that it's running in a browser", function() {
         expect(Csster.browser).not.toBeUndefined();
     });
-    if (/AppleWebKit/.test(AppleWebKit)) {
+    if (/AppleWebKit/.test(navigator.userAgent)) {
         it("should report that it's running in webkit", function() {
             expect(Csster.browser.webkit).not.toBeUndefined();
         });
@@ -98,7 +98,7 @@ describe('userAgent', function() {
             expect(Csster.browser.mozilla).toBeUndefined();
         });
     }
-    if (/^Mozilla/.test(AppleWebKit)) {
+    if (/^Mozilla/.test(navigator.userAgent)) {
         it("should report that it's not running in webkit", function() {
             expect(Csster.browser.webkit).toBeUndefined();
         });
