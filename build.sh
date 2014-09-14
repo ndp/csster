4b36e9981e531314dc16508ad29cac20c0af6a04
Roman Heinrich <roman.heinrich@gmail.com>
2014-09-14 19:53:27 +0200

    remove vendor folder (rails engine)

--- build.sh
+++ build.sh
@@ -9,4 +9,3 @@ echo "// Generated `date`" >> $dist_file
 echo "// "                 >> $dist_file
 echo "// "                 >> $dist_file
 cat src/utils.js src/core.js src/macros/macros.js src/functions/color.js src/filters/property_pre_processors.js src/filters/rule_post_processors.js src/init.js src/jquery.js >> $dist_file
-cp $dist_file vendor/assets/javascripts/
