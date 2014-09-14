cc40687a8c5f0a00f9eaac666accafdf67326495
Roman Heinrich <roman.heinrich@gmail.com>
2014-09-14 20:26:14 +0200

    build version 1.0.5

--- sh/publish.sh
+++ sh/publish.sh
@@ -1,5 +1,10 @@
+#!/bin/sh
 
+current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+topdir="$current_dir/.."
+VERSION=`cat $topdir/VERSION`
 echo "updating to $VERSION"
 git add -A
 git commit -m  "build version $VERSION"
 git tag $VERSION
+git push --tag
