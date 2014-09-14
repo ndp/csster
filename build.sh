ed3f5e010410fb252ec1f4699c2d5d23e7e57f4b
Roman Heinrich <roman.heinrich@gmail.com>
2014-09-14 19:42:00 +0200

    diet folder for the packaged file

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 VERSION="1.0.2"
+dist_file="dist/csster.js"
 
 cat templates/bower.json   | sed "s/__VERSION__/$VERSION/" > bower.json
 cat templates/package.json | sed "s/__VERSION__/$VERSION/" > package.json
-cat templates/header.txt   | sed "s/__VERSION__/$VERSION/" > csster.js
-echo "// Generated `date`" >>csster.js
-echo "// " >>csster.js
-echo "// " >>csster.js
-cat src/utils.js src/core.js src/macros/macros.js src/functions/color.js src/filters/property_pre_processors.js src/filters/rule_post_processors.js src/init.js src/jquery.js >>csster.js
-cp csster.js vendor/assets/javascripts/
+cat templates/header.txt   | sed "s/__VERSION__/$VERSION/" > $dist_file
+echo "// Generated `date`" >> $dist_file
+echo "// " >> $dist_file
+echo "// " >> $dist_file
+cat src/utils.js src/core.js src/macros/macros.js src/functions/color.js src/filters/property_pre_processors.js src/filters/rule_post_processors.js src/init.js src/jquery.js >> $dist_file
+cp $dist_file vendor/assets/javascripts/
--- dist/csster.js
+++ dist/csster.js
@@ -5,7 +5,7 @@
 //
 // See http://github.com/ndp/csster
 //
-// Generated Sun Sep 14 19:36:13 CEST 2014
+// Generated Sun Sep 14 19:41:15 CEST 2014
 // 
 // 
 if (!Csster) {
