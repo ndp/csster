2755b953a071366c60f87eb545521eafcf7c01ae
Andrew Peterson <andy@ndpsoftware.com>
2020-09-15 12:09:00 -0700

    more commits

--- webpack.config.js
+++ webpack.config.js
@@ -10,29 +10,6 @@ module.exports = {
     library: 'Csster',
     libraryTarget: 'umd'
   },
-  resolveLoader: {
-    modulesDirectories: [
-      '../node_modules'
-    ]
-  },
-  module: {
-    resolveLoader: {
-      modulesDirectories: [
-        '../node_modules'
-      ]
-    },
-    loaders: [
-      {
-        test: /\.(es6)$/,
-        exclude: /(node_modules)/,
-        loader: 'babel',
-        query: {
-          presets: ['es2015'],
-          "plugins": ["transform-object-rest-spread"]
-        }
-      }
-    ]
-  }
 }
 
 /*new webpack.optimize.UglifyJsPlugin({
